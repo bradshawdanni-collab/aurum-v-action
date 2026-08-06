66f51c497ee63c1263453c60f725550981cd5af3
Danielle Bradshaw <bradshawdanni@gmail.com>
2026-08-07 01:57:11 +1000

    feat: expose verifier interface

---
+++ src/aurum_v/verification/__init__.py
@@ -0,0 +1,5 @@
+"""Public artifact-verification interface."""
+
+from .verify_artifact import ResultClass, VerificationResult, verify_artifact
+
+__all__ = ["ResultClass", "VerificationResult", "verify_artifact"]
